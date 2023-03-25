f4c529b895a2d825247f8277be5dfb376e9e655f
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-03-25 06:13:48 -1100

    Update The Gauss-Jordan method.cpp

--- The Gauss-Jordan method.cpp
+++ The Gauss-Jordan method.cpp
@@ -1,55 +1,52 @@
-#include <iostream>
-#include <vector>
-
 typedef std::vector<std::vector<double>> Matrix;
 
-std::vector<double> GAUSS_JORDAN_METHOD(Matrrix A, std::vector<double> a)
+std::vector<double> GAUSS_JORDAN_METHOD(Matrix A, std::vector<double> a)
 {
-  int n = b.size();
-  Matrix Aa(n, std::vector<double>(n + 1);
-            
-  for(int i = 0; i < n; i++)
-  {
-    for(int j = 0; j < n; j++)
+    int n = a.size();
+    Matrix Aa(n, std::vector<double>(n + 1));
+
+    for(int i = 0; i < n; i++)
     {
-      Aa[i][j] = A[i][j];
-    }
-    Aa[i][n] = a[i];
-  }
-            
-  for(int i = 0; i < n; i++)
-  {
-    int pivot = i;
-    for(int j = i + 1; j < n; j++)
-    {
-      if(abs(Aa[j][i] > abs(Aa[pivot][i])
-      {
-        pivot = j;
-      }       
-    }
-    if(pivot != i) { swap(Aa[i], Aa[pivot]) }
-    double Pivot = Aa[i][i];
-    for(int j = i; j < n; j++)
-    {
-      Aa[i][j] /= Pivot;
-    }   
-    for(int j = 0; j < n; j++)
-    {
-      if(j != i)
-      {
-        double F = Aa[j][i];
-        for(int k = i; k <= n; k++)
+        for(int j = 0; j < n; j++)
         {
-          Aa[j][k] -= F * Aa[i][k];
-        }  
-      }
-        
+            Aa[i][j] = A[i][j];
+        }
+        Aa[i][n] = a[i];
     }
-  }
-  std::vector<double> x(n);
-  for(int i = 0; i < n; i++)
-  {
-    x[i] = Aa[i][n];
-  }
-  return x;           
+
+    for(int i = 0; i < n; i++)
+    {
+        int pivot = i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(abs(Aa[j][i] > abs(Aa[pivot][i])))
+            {
+                pivot = j;
+            }
+        }
+        if(pivot != i) { swap(Aa[i], Aa[pivot]); }
+        double Pivot = Aa[i][i];
+        for(int j = i; j < n; j++)
+        {
+            Aa[i][j] /= Pivot;
+        }
+        for(int j = 0; j < n; j++)
+        {
+            if(j != i)
+            {
+                double F = Aa[j][i];
+                for(int k = i; k <= n; k++)
+                {
+                    Aa[j][k] -= F * Aa[i][k];
+                }
+            }
+
+        }
+    }
+    std::vector<double> x(n);
+    for(int i = 0; i < n; i++)
+    {
+        x[i] = Aa[i][n];
+    }
+    return x;
 }
