007be4c2297ab0447ad58df8ecffcf7cc23f10f6
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-03-25 04:10:21 -1100

    Create The Gauss-Jordan method.cpp

---
+++ The Gauss-Jordan method.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<std::vector<double>> Matrix;
+
+std::vector<double> GAUSS_JORDAN_METHOD(Matrrix A, std::vector<double> a)
+{
+  int n = b.size();
+  Matrix Aa(n, std::vector<double>(n + 1);
+            
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      Aa[i][j] = A[i][j];
+    }
+    Aa[i][n] = a[i];
+  }
+            
+  for(int i = 0; i < n; i++)
+  {
+    int pivot = i;
+    for(int j = i + 1; j < n; j++)
+    {
+      if(abs(Aa[j][i] > abs(Aa[pivot][i])
+      {
+        
+      }       
+    }
+  }
+}
